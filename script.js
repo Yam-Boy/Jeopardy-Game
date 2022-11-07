43a53722c4e39dc50a99deeeecfab26106fa990a
cherymochi <102265882+cherymochi@users.noreply.github.com>
2022-11-06 20:57:31 -0600

    side menu added

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Main JavaScript file for the application
 
 // Variables
+const mobileMenu = document.querySelector('.mobile-nav');
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
-
-// Animate the hamburger menu
-hamburger.addEventListener('click', function () {
-    this.classList.toggle('is-active');
-});
 
 // Highlight the menu item that is currently active
 menu.addEventListener('click', function () {
     this.classList.toggle('is-active');
+});
+
+// Animate the hamburger menu and show the mobile menu
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+
 });
