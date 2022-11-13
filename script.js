cf84e15c66b6bb7a433f2dbeedb5cd27c060501d
Godyssey <ALEX-ANNSBURRELL@students.utech.edu.jm>
2022-11-12 22:06:46 -0500

    form completion

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ hamburger.addEventListener('click', function () {
     mobileMenu.classList.toggle('is-active');
 
 });
+
+//Function to calculate age based on date of birth
+function calculateAge() {
+    var birthDate = new Date(document.getElementById("dob").value);
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    
+    document.getElementById("age").value = age;
+    if (age >= 12) {
+        return true;
+    } else {
+        alert("You are not eligible to play the game");
+        return false;
+    }    
+}
